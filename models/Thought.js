f7e7edc28f1b09df1af9ed2a1d4d5b6d1a463b4a
NickNocc <haretakis4@gmail.com>
2022-02-18 16:18:35 -0800

    Thought Schema created

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+
+
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => formatDate(createdAtVal)
+        },
+
+        username: {
+            type: String,
+            ref: 'User',
+            required: true
+        },
+
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length
+});
