bb4e4b6434c1192c1d06002db1822585bc0d720a
NickNocc <haretakis4@gmail.com>
2022-02-19 13:33:52 -0800

    update

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -48,6 +48,21 @@ const userController = {
             .catch(err => res.json(err));
     },
 
+    // Update user by id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate(
+        { _id: params.id },
+        body,
+        { new: true, runValidators: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
 
     }
 }
