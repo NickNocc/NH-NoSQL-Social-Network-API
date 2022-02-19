0e8f9dbdb0a9b3d5179a97183eb8a938faaa78b8
NickNocc <haretakis4@gmail.com>
2022-02-18 17:08:14 -0800

    Update user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,22 @@
 const { User, Thought } = require('../models');
 
+const userContoller = {
 
+    getAllUsers(req, res) {
+        User.find({})
+    },
+
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+    },
+
+    createUser({ body }, res) {
+        User.create(body)
+    },
+
+    updateUser({ params, body }, res) {
+        User.findOnAndUpdate
+    }
+}
 
 module.exports = userController;
