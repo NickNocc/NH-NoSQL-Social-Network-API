afca381f814864e05b63a8c0c4397d3dd94eb857
NickNocc <haretakis4@gmail.com>
2022-02-18 17:16:25 -0800

    functions set up

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,21 +1,41 @@
 const { User, Thought } = require('../models');
 
-const userContoller = {
-
+const userController = {
+    // Gets all users
     getAllUsers(req, res) {
         User.find({})
     },
-
+    // Get one user with their id
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
     },
-
+    // Create new user
     createUser({ body }, res) {
         User.create(body)
     },
-
+    // Update user by id
     updateUser({ params, body }, res) {
-        User.findOnAndUpdate
+        User.findOnAndUpdate(
+            { _id: params.id },
+            body,
+            { new: true, runValidators: true }
+        )
+    },
+    // Deletes user by id
+    deleteUser({ params }, res) {
+        User.findOnAndDelete({ _id: params.id })
+    },
+    // Adds friend by id
+    addFriend({ params }, res) {
+        User.findOnAndUpdate(
+            { _id: params.userId },
+            { $push: { friends: params.friendId } },
+            { new: true, runValidators: true }
+        )
+    },
+    // Deletes friend by id
+    deleteFriend({ params }, res) {
+        User.findOnAndDelete({ _id: params.userId })
     }
 }
 
