7989c064ecafd69090eae27e9c315410180d8336
NickNocc <haretakis4@gmail.com>
2022-02-19 14:08:29 -0800

    Update thought-controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,4 +1,32 @@
 const { User, Thought } = require('../models');
 
+const thoughtController = {
+
+    // Gets all thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+    },
+
+    // Get thought by id
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+    },
+
+    // Post new thought
+    // Add Comment
+    addThought({ params, body }, res) {
+        Thought.create(body)
+        
+    },
+
+    // Update thought
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            body,
+            { new: true, runValidators: true }
+        )
+    }
+}
 
 module.exports = thoughtController
