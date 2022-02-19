8e9be595902736d48154881ee315d6ba9f7e4434
NickNocc <haretakis4@gmail.com>
2022-02-18 16:18:21 -0800

    Created User model

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        _id: {
+            type: Schema.ObjectId
+        },
+
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email']
+        },
+
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+
+        friends: [this]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+UserSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+})
+
+const User = model('User', UserSchema);
+
+module.exports = User;
