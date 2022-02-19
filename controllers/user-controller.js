22aa86277ea24dd0655b287327b98a80ac0f69ce
NickNocc <haretakis4@gmail.com>
2022-02-19 13:34:04 -0800

    Update user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -72,6 +72,22 @@ const userController = {
     },
 
     // Adds friend by id
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+        { _id: params.userId },
+        { $push: { friends: params.friendId } },
+        { new: true, runValidators: true }
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err))
+        )
+    },
+
 
     }
 }
