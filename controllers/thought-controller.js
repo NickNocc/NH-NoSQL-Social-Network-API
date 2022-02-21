c9f72ed1302804d34b4c9258e98b47b03176290e
NickNocc <haretakis4@gmail.com>
2022-02-21 13:23:45 -0800

    Create thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -25,7 +25,28 @@ const thoughtController = {
 
     // Post new thought
     // Add Comment
-   
+    createThought({ params, body }, res) {
+        Thought.create(body)
+            .then(({ _id }) => {
+                return User.findOneAndUpdate(
+                    { _id: params.UserId },
+                    { $push: { thoughts: _id } },
+                    { new: true }
+                );
+            })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.json(err));
+        
+    },
+
+    // Update thought
+ 
 };
 
 module.exports = thoughtController
