f3194a5cd30664e484ac8e4dddce7015a7dc6805
NickNocc <haretakis4@gmail.com>
2022-02-21 13:23:55 -0800

    update Thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -46,6 +46,30 @@ const thoughtController = {
     },
 
     // Update thought
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            body,
+            { new: true, runValidators: true })
+            .then(({ _id }) => {
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $push: { thoughts: _id } },
+                    { new: true }
+                );
+            })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.json(err));
+        
+    },
+
+    // Delete thought
  
 };
 
