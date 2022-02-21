5b9d7b9ae7d3b9a5c28852cc2d34dc6e4297b195
NickNocc <haretakis4@gmail.com>
2022-02-21 13:48:41 -0800

    added casing option for email validation

--- models/User.js
+++ models/User.js
@@ -2,9 +2,6 @@ const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema(
     {
-        _id: {
-            type: Schema.ObjectId
-        },
 
         username: {
             type: String,
@@ -17,7 +14,7 @@ const UserSchema = new Schema(
             type: String,
             required: true,
             unique: true,
-            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email']
+            match: [/^([a-zA-Z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email']
         },
 
         thoughts: [
